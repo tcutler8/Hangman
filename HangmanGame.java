831a6c70720ae0f39d5e178e3fa9d092d83a01fa
tcutler <tcutler8@bruinmail.slcc.edu>
2022-04-11 16:52:02 -0600

    revert back to original

--- HangmanGame.java
+++ HangmanGame.java
@@ -3,5 +3,6 @@ import java.util.Scanner;
 public class HangmanGame {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
+
     }
 }
--- Player.java
+++ Player.java
@@ -1,5 +1,3 @@
 public class Player {
-    // here is a test comment
-    // here is another test comment
-    // pretend this is some code
+
 }
