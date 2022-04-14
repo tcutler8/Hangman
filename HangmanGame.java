3cda11e7b41cb5373d7aadde2354e2b0e0dfc487
mjh025 <103548398+mjh025@users.noreply.github.com>
2022-04-14 16:02:36 -0600

    Update HangmanGame.java

--- HangmanGame.java
+++ HangmanGame.java
@@ -2,26 +2,35 @@ import java.util.Scanner;
 
 public class HangmanGame {
     
-    private static String[] words;
-    private static String word = words[(int) (Math.random() * words.length)];
-    private static String underscore = new String(new char[word.length()]).replace("\0", "_");
-    private static int count = 0;
+    private static String currentWord;
+    private static String pastGuesses;
+    private static String currGuesses;
+    private static char guessedLetters;
+    private static int numLettersInWord;
     
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
         
         while (count < 7 && underscore.contains("_")){
             System.out.println("Guess any letter or word");
-            System.out.println(underscore);
-            String guess = scnr.next();
-            hangman(guess);
+            System.out.println(underscore);   //underscores to replace unguessed letters
+            String currentGuess = scnr.next();
+            hangman(currentGuess);
         }
     }
     
-    public static void hangman(String guess) {    
+    public static void startNewGame() {
+        while (startNewGame) {
+            System.out.println("Begin Game!");
+            int currentWord = random.next();  //Will use randomWord from wordList class
+            
+            
+    }  
+    
+    public static void getNewGuesses(String guess) {    
     }
         
-    public static void hangmanGraphic(){
+    public static void updateVisuals() {
         if (count == 1) {
             System.out.println("Wrong. Try again");
             System.out.println("    _______________");
@@ -131,6 +140,26 @@ public class HangmanGame {
             System.out.println("You Lost");
             System.out.println("The word was: " + word);
         }            
-                             
-      }
-}
+    }
+                               
+    public static void updateGuesses(){
+        String currentGuess = scnr.next();
+    }
+                               
+    public static boolean checkWins(){     //Checks player's input for correct word/letters
+        if (guessedLetters == currentWord) {
+            System.out.println("You Won!");
+        } 
+        else {
+            System.out.println("You Lose");
+        }
+      
+    }
+    public static void exit() {            //If player wins or loses, exit question will appear. 
+        if ( && ){
+            System.out.println("Do you want to play again?");
+            if (player              //If player inputs "y", new game starts.
+                                    //If player intpus "n", game will reset and exit
+     
+  }
+ }
