364d6ea16bde30331b207af010897e22e9ddcbff
mjh025 <103548398+mjh025@users.noreply.github.com>
2022-04-13 21:53:53 -0600

    Update HangmanGame.java

--- HangmanGame.java
+++ HangmanGame.java
@@ -1,8 +1,136 @@
 import java.util.Scanner; 
 
 public class HangmanGame {
+    
+    private static String[] words;
+    private static String word = words[(int) (Math.random() * words.length)];
+    private static String underscore = new String(new char[word.length()]).replace("\0", "_");
+    private static int count = 0;
+    
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
-
+        
+        while (count < 7 && underscore.contains("_")){
+            System.out.println("Guess any letter or word");
+            System.out.println(underscore);
+            String guess = scnr.next();
+            hangman(guess);
+        }
     }
+    
+    public static void hangman(String guess) {    
+    }
+        
+    public static void hangmanGraphic(){
+        if (count == 1) {
+            System.out.println("Wrong. Try again");
+            System.out.println("    _______________");
+            System.out.println("    |");
+            System.out.println("    |");
+            System.out.println("    |");
+            System.out.println("    |");
+            System.out.println("    |");
+            System.out.println("    |");
+            System.out.println("    |");
+            System.out.println("    |");
+            System.out.println("    |");
+            System.out.println("    |");
+            System.out.println("____|______");
+        }
+        if (count == 2) {
+            System.out.println("Wrong. Try again"):
+            System.out.println("    _______________");
+            System.out.println("    |            _|_");
+            System.out.println("    |          /      \");
+            System.out.println("    |         |        |");
+            System.out.println("    |          \______/");
+            System.out.println("    |");
+            System.out.println("    |");
+            System.out.println("    |");
+            System.out.println("    |");
+            System.out.println("    |");
+            System.out.println("    |");
+            System.out.println("____|______");
+        }
+        if (count == 3) {
+            System.out.println("Wrong. Try again");
+            System.out.println("    _______________");
+            System.out.println("    |            _|_");
+            System.out.println("    |          /      \");
+            System.out.println("    |         |        |");
+            System.out.println("    |          \______/");
+            System.out.println("    |             |");
+            System.out.println("    |           __|__");
+            System.out.println("    |             | ");
+            System.out.println("    |             |");
+            System.out.println("    |             |");
+            System.out.println("    |");
+            System.out.println("____|______");
+        }
+        if (count == 4) {
+            System.out.println("Wrong. Try again");
+            System.out.println("    _______________");
+            System.out.println("    |            _|_");
+            System.out.println("    |          /      \"); 
+            System.out.println("    |         |        |");
+            System.out.println("    |          \______/");
+            System.out.println("    |             |");
+            System.out.println("    |           __|__");
+            System.out.println("    |          /  |");
+            System.out.println("    |         |   |");
+            System.out.println("    |             |");
+            System.out.println("    |");
+            System.out.println("____|______")
+        }
+        if (count == 5) {
+            System.out.println("Wrong. Try again");
+            System.out.println("    _______________");
+            System.out.println("    |            _|_");
+            System.out.println("    |          /      \");
+            System.out.println("    |         |        |");
+            System.out.println("    |          \______/");
+            System.out.println("    |             |");
+            System.out.println("    |           __|__");
+            System.out.println("    |         /   |    \");
+            System.out.println("    |        |    |     |");
+            System.out.println("    |             |");
+            System.out.println("    |");
+            System.out.println("____|______")
+        }
+        if (count == 6) {
+            System.out.println("Wrong. Try again");
+            System.out.println("Wrong. Try again");
+            System.out.println("    _______________");
+            System.out.println("    |            _|_");
+            System.out.println("    |          /      \");
+            System.out.println("    |         |        |");
+            System.out.println("    |          \______/");
+            System.out.println("    |             |");
+            System.out.println("    |           __|__");
+            System.out.println("    |         /   |    \");
+            System.out.println("    |         |   |     |");
+            System.out.println("    |             |");
+            System.out.println("    |           / ");
+            System.out.println("____|______    /")
+        }
+        if (count == 7) {
+            System.out.println("Wrong. Try again");
+            System.out.println("Wrong. Try again");
+            System.out.println("    _______________");
+            System.out.println("    |            _|_");
+            System.out.println("    |          /      \");
+            System.out.println("    |         |        |");
+            System.out.println("    |          \______/");
+            System.out.println("    |             |");
+            System.out.println("    |           __|__");
+            System.out.println("    |         /   |    \");
+            System.out.println("    |         |   |     |");
+            System.out.println("    |             |");
+            System.out.println("    |           /   \");
+            System.out.println("____|______    /     \");
+            System.out.println("You Lost");
+            System.out.println("The word was: " + word);
+        }            
+                             
+      }
 }
